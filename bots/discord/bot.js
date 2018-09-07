413cb354d11de1d58da6e6872b5d394962762fcd
Travis Hubbard <thubbard@sugarcrm.com>
2018-09-07 10:14:33 -0400

    Discord Bot initial setup

---
+++ bots/discord/bot.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.io');
+const logger = require('winston');
+const auth = require('./auth.json');
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+console.log('running');
+// Initialize Discord Bot
+const bot = new Discord.Client({
+    token: auth.token,
+    autorun: true
+});
+
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        let args = message.substring(1).split(' ');
+        let cmd = args[0];
+
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+                break;
+            // Just add any case commands if you want to..
+        }
+    }
+});
